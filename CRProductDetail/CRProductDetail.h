69b6107c3bcb4eef84cb9da775bb15eb2a492a8b
d2c_cyf <cyfsoftware@126.com>
2017-05-19 14:14:24 +0800

    Initial Commit

---
+++ CRProductDetail/CRProductDetail.h
@@ -0,0 +1,19 @@
+//
+//  CRProductDetail.h
+//  CRProductDetail
+//
+//  Created by d2c_cyf on 17/5/19.
+//  Copyright © 2017年 d2c_cyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRProductDetail.
+FOUNDATION_EXPORT double CRProductDetailVersionNumber;
+
+//! Project version string for CRProductDetail.
+FOUNDATION_EXPORT const unsigned char CRProductDetailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRProductDetail/PublicHeader.h>
+
+
